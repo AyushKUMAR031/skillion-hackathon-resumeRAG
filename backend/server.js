ef158a8b7b22ab1b8b0c1e730ad9da7a4aa6ac24
AyushKUMAR031 <ayushkumar03192003@gmail.com>
2025-10-04 22:06:34 +0530

    Setup express server and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const resumeRoutes = require('./routes/resumes');
+app.use('/api/resumes', resumeRoutes);
+
+app.get('/', (req, res) => {
+  res.send('ResumeRAG API');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
